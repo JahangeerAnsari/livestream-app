40ec51e53e1eab25a27b0ab24104f1e427468d69
jansari <jansari@entiovi.com>
2024-07-18 08:55:48 +0530

    shadcn installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
-  return <h1>Hello from new project</h1>;
+  return (
+    <Button>Hello from liveStream</Button>
+  )
 }
