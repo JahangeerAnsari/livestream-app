be0b4b0656647e9f1dbbb87acf2ef677b52b6ce4
jansari <jansari@entiovi.com>
2024-07-24 11:04:52 +0530

    CRUD (authentication) operations carried out by the clerk

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,10 @@
 // we create prisma client to prevent to the hot reload
 // in production we don't want hot reload
 
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 declare global{
  var prisma: PrismaClient | undefined;
 }
+
 export const db = globalThis.prisma || new PrismaClient();
 if(process.env.NODE_ENV !== 'production') globalThis.prisma = db;
